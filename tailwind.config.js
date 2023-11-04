e7dcc7485e1e7e3ed458410fe4814e7cbfc4caaa
Diego Cuevas <ingdcuevas@gmail.com>
2023-11-03 22:28:00 -0500

    basic configuration finished

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    colors: {
+      darkBlue: "hsl(209, 23%, 22%)",
+      veryDarkBlue: "hsl(207, 26%, 17%)",
+      veryDarkBlueLightText: "hsl(200, 15%, 8%)",
+      darkGray: "hsl(0, 0%, 52%)",
+      veryLightGray: "hsl(0, 0%, 98%)",
+      white: "hsl(0, 0%, 100%)",
+    },
+    fontFamily: {
+      nunito: ["Nunito Sans", "sans-serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
